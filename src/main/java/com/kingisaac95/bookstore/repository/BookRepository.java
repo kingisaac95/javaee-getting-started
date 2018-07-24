212f74a3bff0ccda720e28384fcf7035a07d4706
Orjiewuru Kingdom <kingdom.orjiewuru@andela.com>
2018-07-24 12:43:14 +0100

    add transaction demarcation using JTA

--- src/main/java/com/kingisaac95/bookstore/repository/BookRepository.java
+++ src/main/java/com/kingisaac95/bookstore/repository/BookRepository.java
@@ -5,8 +5,21 @@ import com.kingisaac95.bookstore.model.Book;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
 import java.util.List;
 
+import static javax.transaction.Transactional.TxType.REQUIRED;
+import static javax.transaction.Transactional.TxType.SUPPORTS;
+
+/**
+ * SUPPORTS - best for read-only transactions
+ * if invoked outside a transaction, a new transaction will not be created
+ * but if invoked within a transaction, it'll be executed within the context of
+ * that transaction to allow database isolation
+ * REQUIRED - best for write-only transactions
+ */
+
+@Transactional(SUPPORTS)
 public class BookRepository {
 
   // inject persistence unit
@@ -27,11 +40,13 @@ public class BookRepository {
     return query.getSingleResult();
   }
 
+  @Transactional(REQUIRED)
   public Book create(Book book) {
     em.persist(book);
     return book;
   }
 
+  @Transactional(REQUIRED)
   public void delete(Long id) {
     em.remove(em.getReference(Book.class, id));
   }
