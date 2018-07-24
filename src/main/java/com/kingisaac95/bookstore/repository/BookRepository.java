334e089c92848a875ae1d1db12a7332d29269767
Orjiewuru Kingdom <kingdom.orjiewuru@andela.com>
2018-07-24 12:14:41 +0100

    add JPQL queries to fetch and count all books

--- src/main/java/com/kingisaac95/bookstore/repository/BookRepository.java
+++ src/main/java/com/kingisaac95/bookstore/repository/BookRepository.java
@@ -4,6 +4,8 @@ import com.kingisaac95.bookstore.model.Book;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 public class BookRepository {
 
@@ -13,6 +15,16 @@ public class BookRepository {
 
   public Book find(Long id) {
     return em.find(Book.class, id);
+  }
+
+  public List<Book> findAll() {
+    TypedQuery<Book> query = em.createQuery("select b from Book b order by b.title", Book.class);
+    return query.getResultList();
+  }
+
+  public Long countAll() {
+    TypedQuery<Long> query = em.createQuery("select count(b) from Book b", Long.class);
+    return query.getSingleResult();
   }
 
   public Book create(Book book) {
